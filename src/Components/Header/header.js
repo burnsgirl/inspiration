ee1567f625e4aa4f723a73f8075bc69c6f9a09b2
Dauphine Burns <dauphineburns@gmail.com>
2021-10-25 13:43:02 -0400

    made clouds for nav

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,5 +1,6 @@
 import { React } from "react";
 import "./header.css";
+import cloudd from "../../Assets/nav-cloud.jpeg";
 
 
 
@@ -8,7 +9,27 @@ function Header() {
 
 
     return (
-        <div></div>
+        <div id="header">
+            <ul>
+                <div id="images">
+                    <li>Home</li>
+                </div>
+
+                <div id="images">
+                    <li>Blog</li>
+                </div>
+
+                <div id="images">
+                    <li>Places</li>
+                </div>
+
+                <div id="images">
+                    <li>Guides</li>
+                </div>
+
+                {/* <img className="nav-cloud" src={cloudd} /> */}
+            </ul>
+        </div>
     )
 
 }
