19d8d6c57b3ca95dad3fcb6a84997cd66135437b
Dauphine Burns <dauphineburns@gmail.com>
2021-10-25 10:53:47 -0400

    added images for other 2 sections

--- src/Pages/Home/home.js
+++ src/Pages/Home/home.js
@@ -9,10 +9,21 @@ function Home() {
 
 
     return (
-        <section id="callToAction">
-            <h1>Explore</h1>
-            <p>Where will your next adventure take you? &#8609;</p>
-        </section>
+        <div>
+            <section id="callToAction">
+                <h1>Explore</h1>
+                <p>Where will your next adventure take you? &#8609;</p>
+            </section>
+
+            <section className="sections" id="section2">
+                <h2>Checkout the best places to go in the U.S.</h2>
+                
+            </section>
+
+            <section className="sections" id="section3">
+                <h2>Are you prepared for your trip?</h2>
+            </section>
+        </div>
     )
 
 }
