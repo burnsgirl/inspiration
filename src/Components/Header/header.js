e83d1640e8533e5db90462987ccfbe9da89644ae
Dauphine Burns <dauphineburns@gmail.com>
2021-10-25 14:19:53 -0400

    added routes for header

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,29 +1,48 @@
 import { React } from "react";
 import "./header.css";
 import cloudd from "../../Assets/nav-cloud.jpeg";
+import { useHistory } from "react-router-dom";
 
 
 
 function Header() {
+
+    const history = useHistory();
+
+    const pageRoute2 = () => {
+        history.push("/blog");
+      };
+
+    const pageRoute1 = () => {
+       history.push("/");
+        };
+
+    const pageRoute3 = () => {
+        history.push("/places");
+      };
+
+    const pageRoute4 = () => {
+        history.push("/guides");
+      };
 
 
 
     return (
         <div id="header">
             <ul>
-                <div id="images">
+                <div id="images" onClick={pageRoute1}>
                     <li>Home</li>
                 </div>
 
-                <div id="images">
+                <div id="images" onClick={pageRoute2}>
                     <li>Blog</li>
                 </div>
 
-                <div id="images">
+                <div id="images" onClick={pageRoute3}>
                     <li>Places</li>
                 </div>
 
-                <div id="images">
+                <div id="images" onClick={pageRoute4}>
                     <li>Guides</li>
                 </div>
 
