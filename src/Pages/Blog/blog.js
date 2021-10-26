ad700c2a1b3a4fb63a28de71e1dfc4203da6a1b0
Dauphine Burns <dauphineburns@gmail.com>
2021-10-26 10:30:26 -0400

    created cards for blog

--- src/Pages/Blog/blog.js
+++ src/Pages/Blog/blog.js
@@ -1,13 +1,34 @@
 import { React } from "react";
 import "./blog.css";
 import { useHistory } from "react-router-dom";
+import blog from "../../blog.json";
 
 function Blog() {
 
     const history = useHistory();
 
     return (
-        <div></div>
+        <div>
+
+        <section id="coverBlog">
+            <h1>Follow Where I Go!</h1>
+        </section>
+
+        <section id="blogSection">
+            {blog.map ((props) => {
+                return (
+
+            
+            <div className="blogCard">
+                <h1>{props.date}</h1>
+                <p>{props.content}</p>
+                <img src={props.image} alt={props.alt} />
+            </div>
+                );
+            })}
+        </section>
+
+        </div>
     )
 }
 
