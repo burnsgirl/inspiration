1834535c7f68df95ae9bc35230e34f59c3cc802a
Dauphine Burns <dauphineburns@gmail.com>
2021-10-26 11:09:08 -0400

    btns added

--- src/Pages/Lists/lists.js
+++ src/Pages/Lists/lists.js
@@ -11,6 +11,66 @@ function Lists() {
             <section id="coverLists">
                 <h1>Packing List For Any Occasion!</h1>
             </section>
+
+            <section id="listBody">
+                <div id="listBtns">
+
+                    <div class="dropdown">
+                        <button class="dropbtn">Hiking</button>
+                        <div class="dropdown-content">
+                            <a href="#">Spring/Summer</a>
+                            <a href="#">Fall/Winter</a>
+                        </div>
+                    </div>
+
+                    <div class="dropdown">
+                        <button class="dropbtn">Beach</button>
+                        <div class="dropdown-content">
+                            <a href="#">Spring/Summer</a>
+                            <a href="#">Fall/Winter</a>
+                        </div>
+                    </div>
+
+                    <div class="dropdown">
+                        <button class="dropbtn">Mountains</button>
+                        <div class="dropdown-content">
+                            <a href="#">Spring/Summer</a>
+                            <a href="#">Fall/Winter</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
+
+                    <div class="dropdown">
+                        <button class="dropbtn">Camping</button>
+                        <div class="dropdown-content">
+                            <a href="#">Spring/Summer</a>
+                            <a href="#">Fall/Winter</a>
+                            <a href="#">Link 3</a>
+                        </div>
+                    </div>
+
+                    <div class="dropdown">
+                        <button class="dropbtn">International</button>
+                        <div class="dropdown-content">
+                            <a href="#">Spring</a>
+                            <a href="#">Summer</a>
+                            <a href="#">Fall</a>
+                            <a href="#">Winter</a>
+                        </div>
+                    </div>
+
+                    <div class="dropdown">
+                        <button class="dropbtn">Desert</button>
+                        <div class="dropdown-content">
+                            <a href="#">Spring</a>
+                            <a href="#">Summer</a>
+                            <a href="#">Fall</a>
+                            <a href="#">Winter</a>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
         </div>
     )
 }
