34c1e937896d4040bdad397dc7f2260d73ae967d
Dauphine Burns <dauphineburns@gmail.com>
2021-10-27 19:26:19 -0400

    added routes for lists

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import Blog from "./Pages/Blog/blog";
 import Guides from "./Pages/Guides/guides";
 import Lists from "./Pages/Lists/lists";
 import Places from "./Pages/Places/places";
+import Beach from "./Pages/Lists/Beach/beach";
+import Camping from "./Pages/Lists/Camping/camping";
+import Desert from "./Pages/Lists/Desert/desert";
+import Hiking from "./Pages/Lists/Hiking/hiking";
+import International from "./Pages/Lists/International/international";
+import Mountains from "./Pages/Lists/Mountains/mountains";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
@@ -21,6 +27,12 @@ function App() {
       <Route exact path="/guides" component={Guides} />
       <Route exact path="/lists" component={Lists} />
       <Route exact path="/places" component={Places} />
+      <Route exact path="/beach" component={Beach} />
+      <Route exact path="/camping" component={Camping} />
+      <Route exact path="/desert" component={Desert} />
+      <Route exact path="/hiking" component={Hiking} />
+      <Route exact path="/international" component={International} />
+      <Route exact path="/mountains" component={Mountains} />
 
       </Switch>
       <Footer />
