891a582d9cb833a54fe09079663021fbbb9b2295
Dauphine Burns <dauphineburns@gmail.com>
2021-10-27 08:58:49 -0400

    have card layout

--- src/Pages/Guides/guides.js
+++ src/Pages/Guides/guides.js
@@ -7,7 +7,47 @@ function Guides() {
     const history = useHistory();
 
     return (
-        <div></div>
+        <div>
+            <section id="guidesLists">
+                <h1>Guides</h1>
+            </section>
+
+            <section id="guidesBody">
+                <div>
+                    <h1>The Place To Start If You Don't Know Where To Start!</h1>
+                </div>
+
+                <div id="guideCards">
+
+                    <div id="guideCard1">
+                        <img id="card11" />
+                        <p>Location</p>
+                        <img id="card12" />
+                        <p>Weather</p>
+                    </div>
+
+                    <div id="guideCard2">
+                        <img id="card21"/>
+                        <p>Lists</p>
+                        <img id="card22"/>
+                        <p>How To Get There</p>
+                        <img id="card23"/>
+                        <p>Pet Friendly</p>
+                    </div>
+
+                    <div id="guideCard3">
+                        <img id="card31"/>
+                        <p>What To Do</p>
+                        <img id="card32"/>
+                        <p>Kid Friendly</p>
+                    </div>
+
+                </div>
+
+            </section>
+
+
+        </div>
     )
 }
 
