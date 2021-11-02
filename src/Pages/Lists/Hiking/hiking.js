ecf97efb92b07a1a91b405fbeb151cfb3decb7dc
Dauphine Burns <dauphineburns@gmail.com>
2021-11-02 14:48:50 -0400

    changed root and zindex for places

--- src/Pages/Lists/Hiking/hiking.js
+++ src/Pages/Lists/Hiking/hiking.js
@@ -7,26 +7,28 @@ function Hiking() {
     const history = useHistory();
 
     return (
-        <div>
+        <div id="bodyHiking">
             <section id="hikingLists">
                 <h1>Hiking List</h1>
             </section>
 
-            <section>
-                <h1>Tips</h1>
-                <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
-            </section>
+           
+                <section className="hikingBody">
+                    <h1>Tips</h1>
+                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+                </section>
 
-            <section>
-                <form action="/action_page.php">
-                <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
-                <label for="vehicle1">Backpack</label>
-                <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
-                <label for="vehicle2">Hiking Boots</label>
-                <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat" />
-                <label for="vehicle3">Water</label>
-                </form>
-            </section>
+                <section className="hikingBody">
+                    <form action="/action_page.php">
+                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                    <label for="vehicle1">Backpack</label>
+                    <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
+                    <label for="vehicle2">Hiking Boots</label>
+                    <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat" />
+                    <label for="vehicle3">Water</label>
+                    </form>
+                </section>
+            
 
         </div>
     );
