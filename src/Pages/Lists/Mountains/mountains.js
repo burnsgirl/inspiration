5e84e20079199ad115966811874063ba57c6915d
Dauphine Burns <dauphineburns@gmail.com>
2021-11-02 17:33:07 -0400

    added grid layout to all list;

--- src/Pages/Lists/Mountains/mountains.js
+++ src/Pages/Lists/Mountains/mountains.js
@@ -8,24 +8,81 @@ function Mountains() {
 
     return (
         <div>
-             <section id="guidesLists">
+             <section id="mountainsLists">
                 <h1>Mountains List</h1>
             </section>
 
-            <section>
+            <section className="listBody" id="tips">
                 <h1>Tips</h1>
                 <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
             </section>
 
-            <section>
-                <form action="/action_page.php">
-                <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
-                <label for="vehicle1"> I have a bike</label>
-                <input type="checkbox" id="vehicle2" name="vehicle2" value="Car" />
-                <label for="vehicle2"> I have a car</label>
-                <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat" />
-                <label for="vehicle3"> I have a boat</label>
-                </form>
+            <section className="listBody" id="tips">
+                    <h1>Always Bring:</h1>
+            </section>
+
+            <section className="grid-container">
+                    
+                        <div>
+                            <input type="checkbox" name="backpack" value="Backpack" />
+                            <label for="backpack" className="inputs">Backpack</label>
+                        </div>
+                        <div>
+                            <input type="checkbox"  name="boots" value="Boots" />
+                            <label for="boots" className="inputs">Hiking Boots</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="water" value="Water" />
+                            <label for="water" className="inputs">Water</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="Food" value="Food" />
+                            <label for="Food" className="inputs">Food</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="FirstAidKit" value="FirstAidKit" />
+                            <label for="FirstAidKit" className="inputs">First Aid Kit</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="Map" value="Map" />
+                            <label for="Map" className="inputs">Map</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="Compass" value="Compass" />
+                            <label for="Compass" className="inputs">Compass</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="Sunscreen" value="Sunscreen" />
+                            <label for="Sunscreen" className="inputs">Sunscreen</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="Hat" value="Hat" />
+                            <label for="Hat" className="inputs">Hat</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="HandSanitizer" value="HandSanitizer" />
+                            <label for="HandSanitizer" className="inputs">Hand Sanitizer</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="BugSpray" value="BugSpray" />
+                            <label for="BugSpray" className="inputs">Bug Spray</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="Knife" value="Knife" />
+                            <label for="Knife" className="inputs">Knife</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="HeadLamp" value="HeadLamp" />
+                            <label for="HeadLamp" className="inputs">Head Lamp</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="Lighter" value="Lighter" />
+                            <label for="Lighter" className="inputs">Lighter</label>
+                        </div>
+                        <div>
+                            <input type="checkbox" name="Jacket" value="Jacket" />
+                            <label for="Jacket" className="inputs">Jacket</label>
+                        </div>
             </section>
             
         </div>
